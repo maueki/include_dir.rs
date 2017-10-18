a739844a8a88b18a03fc4d6f13a83b50bd5110d5
Ueki Masaru <masaru.ueki@itage.co.jp>
2017-10-18 18:20:10 +0900

    Minor fix

--- tests/include_dir_test.rs
+++ tests/include_dir_test.rs
@@ -2,7 +2,7 @@
 #![plugin(include_dir)]
 
 #[test] fn include_dir_test() {
-    let hoge = include_dir!("");
-    let content: String = String::from_utf8(hoge.get("dir/a.txt").unwrap().to_vec()).unwrap();
+    let file_map = include_dir!("");
+    let content: String = String::from_utf8(file_map.get("dir/a.txt").unwrap().to_vec()).unwrap();
     assert_eq!(content, "hoge\n");
 }
